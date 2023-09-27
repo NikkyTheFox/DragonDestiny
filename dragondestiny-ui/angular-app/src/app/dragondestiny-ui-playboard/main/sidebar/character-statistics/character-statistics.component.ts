b3d76146259dc0199c1882b985d25ba486df16d0
NikkyTheFox <adi.pochec@gmail.com>
2023-09-27 19:17:54 +0200

    quick fix

--- dragondestiny-ui/angular-app/src/app/dragondestiny-ui-playboard/main/sidebar/character-statistics/character-statistics.component.ts
+++ dragondestiny-ui/angular-app/src/app/dragondestiny-ui-playboard/main/sidebar/character-statistics/character-statistics.component.ts
@@ -1,11 +1,25 @@
-import { Component, Input } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import { PlayedGameCharacter } from '../../../../interfaces/played-game/character/character';
+import {Player} from "../../../../interfaces/played-game/player/player";
+import {PlayedGameService} from "../../../../services/played-game/played-game-service";
+import {RequestStructureGameidPlayerlogin} from "../../../../interfaces/request-structure-gameid-playerlogin";
 
 @Component({
   selector: 'app-character-statistics',
   templateUrl: './character-statistics.component.html',
   styleUrls: ['./character-statistics.component.css']
 })
-export class CharacterStatisticsComponent {
+export class CharacterStatisticsComponent implements OnInit {
   @Input() character!: PlayedGameCharacter;
+  @Input() requestStructure!: RequestStructureGameidPlayerlogin;
+  player!: Player;
+
+  constructor(private playedGameService: PlayedGameService) {
+  }
+   ngOnInit() {
+    this.playedGameService.getPlayer(this.requestStructure.gameId, this.requestStructure.playerLogin).subscribe( (data: Player) => {
+      this.player = data;
+    });
+   }
+
 }
