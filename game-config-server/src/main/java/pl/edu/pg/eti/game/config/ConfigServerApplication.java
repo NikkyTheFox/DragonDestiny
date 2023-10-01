3984befb514bbb862ba9711ad93cd20f73a67671
Igor Szemela <s184659@student.pg.edu.pl>
2023-10-01 23:18:26 +0200

    Dockerized game_config_server

--- game-config-server/src/main/java/pl/edu/pg/eti/game/config/ConfigServerApplication.java
+++ game-config-server/src/main/java/pl/edu/pg/eti/game/config/ConfigServerApplication.java
@@ -3,12 +3,14 @@ package pl.edu.pg.eti.game.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Config Server will maintain all configuration of the microservices.
  */
 
 @EnableConfigServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigServerApplication {
 
